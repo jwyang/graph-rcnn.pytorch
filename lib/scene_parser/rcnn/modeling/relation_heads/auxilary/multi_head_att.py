c19fff42baecf117af368571a993f1a22bf1320f
jwyang <jw2.yang@gmail.com>
2020-02-19 02:05:47 -0500

    add relationshipness computation v2

---
+++ lib/scene_parser/rcnn/modeling/relation_heads/auxilary/multi_head_att.py
@@ -0,0 +1,58 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def attention(q, k, v, d_k, mask=None, dropout=None):
+
+    scores = torch.matmul(q, k.transpose(-2, -1)) /  math.sqrt(d_k)
+    if mask is not None:
+        mask = mask.unsqueeze(1)
+        scores = scores.masked_fill(mask == 0, -1e9)
+
+    scores = F.softmax(scores, dim=-1)
+
+    if dropout is not None:
+        scores = dropout(scores)
+
+    output = torch.matmul(scores, v)
+    return output
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, heads, d_model, dropout = 0.1):
+        super().__init__()
+
+        self.d_model = d_model
+        self.d_k = d_model // heads
+        self.h = heads
+
+        self.q_linear = nn.Linear(d_model, d_model)
+        self.v_linear = nn.Linear(d_model, d_model)
+        self.k_linear = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.out = nn.Linear(d_model, d_model)
+
+    def forward(self, q, k, v, mask=None):
+
+        bs = q.size(0)
+
+        # perform linear operation and split into h heads
+
+        k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
+        q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
+        v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
+
+        # transpose to get dimensions bs * h * sl * d_model
+
+        k = k.transpose(1,2)
+        q = q.transpose(1,2)
+        v = v.transpose(1,2)
+
+        # calculate attention using function we will define next
+        scores = attention(q, k, v, self.d_k, mask, self.dropout)
+        # concatenate heads and put through final linear layer
+        concat = scores.transpose(1,2).contiguous().view(bs, -1, self.d_model)
+
+        output = self.out(concat)
+
+        return output
--- lib/scene_parser/rcnn/modeling/relation_heads/relpn/relationshipness.py
+++ lib/scene_parser/rcnn/modeling/relation_heads/relpn/relationshipness.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from .utils import box_pos_encoder
+from ..auxilary.multi_head_att import MultiHeadAttention
 
 class Relationshipness(nn.Module):
     """
@@ -44,6 +45,68 @@ class Relationshipness(nn.Module):
             pos = box_pos_encoder(bbox, imsize[0], imsize[1])
             pos_subj = self.sub_pos_encoder(pos)
             pos_obj = self.obj_pos_encoder(pos)
+            pos_scores = torch.mm(pos_subj, pos_obj.t()) # k x k
+            scores = scores + pos_scores
+        relness = torch.sigmoid(scores)      # k x k
+        return relness
+
+class Relationshipnessv2(nn.Module):
+    """
+    compute relationshipness between subjects and objects
+    """
+    def __init__(self, dim, pos_encoding=False):
+        super(Relationshipnessv2, self).__init__()
+
+        self.subj_proj = nn.Sequential(
+            nn.Linear(dim, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 64)
+        )
+
+        self.obj_proj = nn.Sequential(
+            nn.Linear(dim, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 64)
+        )
+
+        self.pos_encoding = False
+        if pos_encoding:
+            self.pos_encoding = True
+            self.sub_pos_encoder = nn.Sequential(
+                nn.Linear(6, 64),
+                nn.ReLU(True),
+                nn.Linear(64, 64)
+            )
+
+            self.obj_pos_encoder = nn.Sequential(
+                nn.Linear(6, 64),
+                nn.ReLU(True),
+                nn.Linear(64, 64)
+            )
+
+        # using context to modulate the relationshipness scores
+        self.self_att_subj = MultiHeadAttention(8, 64)
+        self.self_att_obj = MultiHeadAttention(8, 64)
+
+        self.self_att_pos_subj = MultiHeadAttention(8, 64)
+        self.self_att_pos_obj = MultiHeadAttention(8, 64)
+
+    def forward(self, x, bbox=None, imsize=None):
+        x_subj = self.subj_proj(x) # k x 64
+        x_subj = self.self_att_subj(x_subj, x_subj, x_subj).squeeze(1)
+
+        x_obj = self.obj_proj(x)   # k x 64
+        x_obj = self.self_att_obj(x_obj, x_obj, x_obj).squeeze(1)
+
+        scores = torch.mm(x_subj, x_obj.t()) # k x k
+        if self.pos_encoding:
+            pos = box_pos_encoder(bbox, imsize[0], imsize[1])
+            pos_subj = self.sub_pos_encoder(pos)
+            pos_subj = self.self_att_pos_subj(pos_subj, pos_subj, pos_subj).squeeze(1)
+
+            pos_obj = self.obj_pos_encoder(pos)
+            pos_obj = self.self_att_pos_obj(pos_obj, pos_obj, pos_obj).squeeze(1)
+
             pos_scores = torch.mm(pos_subj, pos_obj.t()) # k x k
             scores = scores + pos_scores
         relness = torch.sigmoid(scores)      # k x k
